c88157bcac918baff48c14e6cbe29775e51e6a63
“Felix <forsethf@gmail.com>
2023-09-14 11:41:42 +0200

    Added plotting test

---
+++ prosjekt_1/test_plot.py
@@ -0,0 +1,24 @@
+from functionality import *
+import matplotlib.pyplot as plt
+
+n = 100
+x = np.arange(0, 1, 0.05)
+y = np.arange(0, 1, 0.05)
+x, y = np.meshgrid(x,y)
+z = FrankeFunction(x, y)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+# Customize the z axis.
+ax.set_zlim(-0.10, 1.40)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
