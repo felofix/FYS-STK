b6719965997f7d7b806631e9981003864c94a038
“Felix <forsethf@gmail.com>
2023-09-15 12:58:13 +0200

    schmoney

--- prosjekt_1/ridge_plotting.py
+++ prosjekt_1/ridge_plotting.py
@@ -15,26 +15,24 @@ y_flat = y.flatten()
 z_flat = z.flatten()
 
 # Creating design matrix. 
-nr_of_degrees = 5
-R2_scores = np.zeros(nr_of_degrees)
-MSE_scores = np.zeros(nr_of_degrees)
+nr_of_degrees = 10
+lambdas = [0.0001, 0.001, 0.01, 0.1, 1]
+
+R2_scores = np.zeros((nr_of_degrees, len(lambdas)))
+MSE_scores = np.zeros((nr_of_degrees, len(lambdas)))
 
 x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x_flat, y_flat, z_flat, test_size=0.2)
 
-for degree in range(1, nr_of_degrees+1):
-	reg = Franke_Regression()
-	X_train = reg.create_desgin_matrix(x_train, y_train, degree)
-	X_scaled = reg.mean_scale(X_train)
+for l in range(len(lambdas)):
+	for degree in range(1, nr_of_degrees+1):
+		reg = Franke_Regression()
+		X_train = reg.create_desgin_matrix(x_train, y_train, degree)
+		X_scaled = reg.mean_scale(X_train)
+		betas = reg.find_betas_Ridge(X_scaled, z_train, lambdas[l])
+		z_pred = reg.predict_z(X_scaled, betas)
+		R2_scores[degree - 1, l] = reg.R2_score(z_train, z_pred)
+		MSE_scores[degree - 1, l] = reg.MSE(z_train, z_pred)
 
-	betas = reg.find_betas_Ridge(X_scaled, z_train, 1)
-	clf = Ridge(alpha=1, fit_intercept=False)
-	clf.fit(X_scaled, z_train)
-
-	z_pred = reg.predict_z(X_scaled, betas)
-	z_pred_sklearn = clf.predict(X_scaled)
-
-	R2_scores[degree - 1] = reg.R2_score(z_train, z_pred)
-	MSE_scores[degree - 1] = reg.MSE(z_train, z_pred)
 
 all_degrees = np.arange(1,nr_of_degrees+1, 1)
 plt.plot(all_degrees, MSE_scores, label = "MSE")
